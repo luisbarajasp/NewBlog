a1d63ad4c96dc7ab926182d020affd419bc83c0f
luisbarajasp <luisbarajasp@gmail.com>
2015-11-12 22:02:14 -0600

    Add name and nickname to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113025545) do
+ActiveRecord::Schema.define(version: 20151113034318) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20151113025545) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "nickname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
