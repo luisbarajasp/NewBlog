1fa6d5d68bab6556943e8cb8b4d1a2f8ad501c7b
luisbarajasp <luisbarajasp@gmail.com>
2015-11-23 16:57:48 -0600

    Add users#show for like a profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   #, :skip => :registrations
+  #get 'users/:id' => 'users#show', as: 'user'
+  #get 'users/index'
+  resources :users, :only => [:show,:index]
+
   resources :posts
   get 'welcome/index'
   root 'welcome#index'
