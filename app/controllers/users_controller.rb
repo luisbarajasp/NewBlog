9eb870c9d6182c22047dd3a7d615a5c6b57cb85d
luisbarajasp <luisbarajasp@gmail.com>
2015-11-25 12:24:46 -0600

    Upgrade the users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
   	@user = User.find(params[:id])
+    @posts = @user.posts.order("created_at DESC")
 
   	respond_to do |format|
         format.html # show.html.erb
