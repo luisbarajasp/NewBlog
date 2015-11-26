c4e1bc39346090b291eb2e0a864b5583e9b1f114
luisbarajasp <luisbarajasp@gmail.com>
2015-11-26 11:35:26 -0600

    Add categories#index, #edit, #destroy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,11 @@
 class CategoriesController < ApplicationController
+
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    @categories = Category.all.order("category ASC")
+  end
+
   def new
   	@category = Category.new
   end
@@ -8,9 +15,34 @@ class CategoriesController < ApplicationController
 
   	if @category.save
 		redirect_to posts_path, notice: "Liga guardada satisfactoriamente"
-	else
-		render 'new', notice: "Error: no se pudo guardar"
-	end
+  	else
+  		render 'new', notice: "Error: no se pudo guardar"
+  	end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    respond_to do |f|
+      if @category.update(category_params)
+        f.html {redirect_to categories_path, notice: "Tu liga se guardo satisfactoriamente con el nuevo material"}
+        f.json { render :show, status: :ok, location: @category }
+      else
+        f.html { render :edit }
+        f.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    if @category.present?
+      @category.destroy
+    end
+    redirect_to categories_path
   end
 
   def show
@@ -18,7 +50,7 @@ class CategoriesController < ApplicationController
     @posts = @category.posts.order("created_at DESC").paginate(page: params[:page], per_page: 10)
     @users = User.all
     @categories = Category.all.order("category ASC")
-    
+
   	respond_to do |format|
         format.html # show.html.erb
         format.xml { render :xml => @category }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :categories, :only => [:new,:show,:create]
+  resources :categories
 
   devise_for :users
   #, :skip => :registrations
